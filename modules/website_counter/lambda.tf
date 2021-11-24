aa98f811207ccc5c0765481a2b6051e4b393cf29
tbojustin <tbojustin@users.noreply.github.com>
2021-11-24 17:34:07 -0500

    moved references to something more modern

--- modules/website_counter/lambda.tf
+++ modules/website_counter/lambda.tf
@@ -77,22 +77,22 @@ module "iam_role" {
 }
 
 resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.counter_api.id}"
-  parent_id   = "${aws_api_gateway_rest_api.counter_api.root_resource_id}"
+  rest_api_id = aws_api_gateway_rest_api.counter_api.id
+  parent_id   = aws_api_gateway_rest_api.counter_api.root_resource_id
   path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = "${aws_api_gateway_rest_api.counter_api.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
+  rest_api_id   = aws_api_gateway_rest_api.counter_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = "${aws_api_gateway_rest_api.counter_api.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.counter_api.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY" 
@@ -100,16 +100,16 @@ resource "aws_api_gateway_integration" "lambda" {
 }
 
 resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = "${aws_api_gateway_rest_api.counter_api.id}"
-  resource_id   = "${aws_api_gateway_rest_api.counter_api.root_resource_id}"
+  rest_api_id   = aws_api_gateway_rest_api.counter_api.id
+  resource_id   = aws_api_gateway_rest_api.counter_api.root_resource_id
   http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.counter_api.id}"
-  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.counter_api.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
